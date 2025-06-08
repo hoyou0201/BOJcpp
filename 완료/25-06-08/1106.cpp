6415c9bbd870cf9c528aa02501c28e7d75bb3f4a
ChoiHoSeong <chs3112a@gmail.com>
2025-06-08 06:30:58 +0000

    1106번 풀이 완료

--- 완료/25-06-08/1106.cpp
+++ 완료/25-06-08/1106.cpp
@@ -3,13 +3,16 @@
 #include <algorithm>
 using namespace std;
 
-int n, c;
-int dp[1005];
+int n, c, m, mm;
+vector<int> dp;
 vector<pair<int,int>> a;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
+
+    dp = vector<int>(2000, 1000000);
+    dp[0] = 0;
 
     cin >> c >> n;
 
@@ -17,21 +20,25 @@ int main() {
     
     for (int i = 0; i < n; i++) {
         cin >> a[i].first >> a[i].second;
+        m = max(m, a[i].second);
     }
 
-    for (int i = 1; i <= c; i++) {
+    for (int i = 1; i <= c+m; i++) {
         for (int j = 0; j < n; j++) {
             if(i-a[j].second >= 0){
-                if(dp[i] == 0){
-                    dp[i] = dp[i-a[j].second]+a[j].first;
-                }
-                else{
-                    dp[i] = min(dp[i], dp[i-a[j].second]+a[j].first);
-                }
+                dp[i] = min(dp[i], dp[i-a[j].second]+a[j].first);
+            }
+        }
+        if(i >= c){
+            if(mm == 0){
+                mm = dp[i];
+            }
+            else{
+                mm = min(dp[i], mm);
             }
         }
     }
-    cout << dp[c];
+    cout << mm;
     
 
     return 0;
