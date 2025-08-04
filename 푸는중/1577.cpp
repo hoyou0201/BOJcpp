aea89a33d42db7f1aaf1b4cb02988e7036cf2d24
ChoiHoSeong <chs3112a@gmail.com>
2025-08-04 07:55:41 +0000

    1577번 풀다 맘

---
+++ 푸는중/1577.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m, k;
+vector<vector<int>> r;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> m >> k;
+
+
+    return 0;
+}
