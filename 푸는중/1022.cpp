05b810e71510de81906d90937ceb8b466058a3ce
ChoiHoSeong <chs3112a@gmail.com>
2025-08-18 08:18:22 +0000

    1022번 풀다 맘

---
+++ 푸는중/1022.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int r1, c1, r2, c2;
+vector<vector<int>> mmm;
+int mj;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> r1 >> c1 >> r2 >> c2;
+    mmm = vector<vector<int>>();
+    mj = 0;
+
+    int ii = 0;
+    for(int i = r1; i <= r2; i++){
+        mmm.push_back(vector<int>());
+        for(int j = c1; j <= c2; j++){
+            int k = max(abs(i), abs(j));
+            int g = (2*k+1)*(2*k+1);
+            int t;
+            if(i == k){
+                t = g-k+j;
+            }
+            else if(j == -k){
+                t = g-3*k+j;
+            }
+            else if(i == -k){
+                t = g-5*k+j;
+            }
+            else{
+                t = g-7*k+j;
+            }
+            mj = max(mj, t%10);
+            mmm[ii].push_back(t);
+        }
+        ii++;
+    }
+
+    for(auto i : mmm){
+        for(int j : i){
+            int s = j%10;
+            for(int k = s; k < mj; k++){
+                cout << ' ';
+            }
+            cout << j;
+        }
+        cout << '\n';
+    }
+    return 0;
+}
